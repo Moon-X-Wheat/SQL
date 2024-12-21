b02f5ecd1c035a6786de48afa54faf938ee0c211
Moon-X-Wheat <Pikku_M@outlook.com>
2024-12-21 17:59:46 +0800

    Update lec1.sql

--- cs 50/Lecture/lec1.sql
+++ cs 50/Lecture/lec1.sql
@@ -1,6 +1,10 @@
-- relational database: there are relationships between tables and tables within database
+-- relational database: there are relationships between tables and tables within database
 /*
 one by one
 one by many
 many by many
+
+Primary Key：给某些表格的数据赋予一个唯一的标识符，数据就可以在表格之间"游走"
+ - 主键和外键：当主键游走到其它表格中的时候，就变成了外键
+ - 不同表的主键可以作为外键相互结合，形成一个新的表。（方便解决一对多或者多对多的问题）
 */
