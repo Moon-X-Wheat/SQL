26d55274a1221779b0de8f794a829a886e90b37a
Moon-X-Wheat <Pikku_M@outlook.com>
2024-12-29 15:25:42 +0800

    Update Moneyball.sql

--- cs 50/Pset/1/Moneyball.sql
+++ cs 50/Pset/1/Moneyball.sql
@@ -32,3 +32,60 @@ join salaries on "players"."id" = "salaries"."player_id"
 WHERE year = 2001
 ORDER by salary, first_name, last_name, player_id
 limit 50;
+
+-- 5. all teams SP played for, only teams' names
+select name
+FROM teams
+WHERE id in (
+  SELECT DISTINCT team_id
+  FROM performances
+  WHERE player_id = (
+    SELECT id
+    FROM players
+    WHERE first_name = 'Satchel'
+  )
+)
+
+-- 6. the top 5 teams, sorted by the total number of hits players 2001: column name: totals hits; 
+SELECT "teams"."name", SUM("performances"."H") as "total hits"
+FROM teams
+JOIN performances on "teams"."id" = "performances"."team_id"
+WHERE "performances"."year" = 2001
+GROUP by "teams"."name"
+order by "total hits" DESC
+limit 5;
+
+-- 7. player who's been paid the highest salary, of all time: two columns: first name and last name
+SELECT first_name, last_name
+FROM players
+WHERE id = (
+  SELECT player_id
+  FROM salaries
+  GROUP by player_id
+  HAVING SUM(salary) = (
+    SELECT  SUM(salary) as "total salary"
+    FROM salaries
+    GROUP by player_id
+    ORDER by "total salary" DESC
+    LIMIT 1
+  )
+)
+
+-- 8. 2001 salary of the player who hit the most home runs in 2001: only salary
+WITH "most home runs" as (
+SELECT player_id, SUM(h) as "total hits"
+FROM performances
+group by player_id
+HAVING year = 2001
+ORDER by "total hits" Desc
+LIMIT 1
+)
+SELECT salary
+from salaries
+WHERE player_id = (
+  SELECT "player_id"
+  from "most home runs"
+);
+
+-- 9. 5 lowest paying teams (by average salary) in 2001: round the average salary to two decimal places and call it "average salary:; sort by average salary, least to greatest; two columns: names and average salary
+
