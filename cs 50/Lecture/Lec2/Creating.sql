d660ce31c7b221532ae02bfec6b3b0c78c679301
Moon-X-Wheat <Pikku_M@outlook.com>
2025-01-03 19:52:14 +0800

    Update Creating.sql

--- cs 50/Lecture/Lec2/Creating.sql
+++ cs 50/Lecture/Lec2/Creating.sql
@@ -29,9 +29,33 @@ CREATE TABLE "station" (
 -- column constraint
 -- not null 非空
 -- unique 不可重复
+-- check  设置条件
+-- default  设置初始值
 CREATE TABLE "station" (
     "id" integer,
     "name" text not null UNIQUE,
     "line" text not null,
     PRIMARY key ("id")
   );
+
+CREATE TABLE "swipe" (
+  "id" integer,
+  "card_id" integer,
+  "station_id" integer,
+  "type" text not null CHECK("type" in ('enter', 'exit', 'deposit')), -- 设置条件
+  "datatime" numeric not null DEFAULT CURRENT_TIMESTAMP, -- 设置初始值
+  "amount" numeric not null CHECK("amount" !=0),
+  PRIMARY key ("id")
+  FOREIGN key ("card_id") REFERENCES "card"("id"),
+  FOREIGN key ("station_id") REFERENCES "station"("id")
+)
+
+-- 修改表格
+-- drop table
+-- rename table
+ALTER TABLE "visits" 
+RENAME to "swipes";
+-- rename column
+ALTER TABLE "swipes" 
+RENAME COLUMN "id" to "bala"
+
