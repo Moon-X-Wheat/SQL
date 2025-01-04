f720cd5921362d4e9bc161c2ff98702aff0aacc3
Moon-X-Wheat <Pikku_M@outlook.com>
2025-01-04 19:41:20 +0800

    Update schema.sql

--- cs 50/Pset/2/schema.sql
+++ cs 50/Pset/2/schema.sql
@@ -1,1 +1,40 @@
+create table "passenger" (
+  "id" integer,
+  "first_name" text not null,
+  "last_name" text not null,
+  "age" integer not null,
+  PRIMARY key ("id")
+  );
+  
+create table "check-ins" (
+  "id" integer,
+  "passenger_id" integer,
+  "time" numeric not null,
+  "flight_id" integer,
+  PRIMARY key ("id"),
+  FOREIGN key ("flight_id") REFERENCES "flights"("id")
+  FOREIGN key ("passenger_id") REFERENCES "passenger"("id")
+ );
+ 
+ create table "airlines" (
+   "id" integer,
+   "name" text not null UNIQUE,
+   "concourse" text not null,
+   PRIMARY key ("id")
+  );
+  
+  create table "flight" (
+    "id" integer,
+    "flight number" integer not null,
+    "airline_id" integer,
+    "departing airport code" text not null,
+    "heading airport code" text not null,
+    "departure time" numeric not null,
+    "arrival time" numeric not null,
+    PRIMARY key ("id"),
+    FOREIGN key ("airline_id") REFERENCES "airlines"("id")
+  );
+  
+  
+
 
