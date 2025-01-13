51f53bbe4e1113c6e080c34ebcb72615e0f0c546
Moon-X-Wheat <Pikku_M@outlook.com>
2025-01-13 20:21:15 +0800

    Update viewing.sql

--- cs 50/Lecture/Lec4/viewing.sql
+++ cs 50/Lecture/Lec4/viewing.sql
@@ -15,3 +15,38 @@ group by book_id
 -- 更新基础数据，视图中的数据也会随之更新
 
 -- 从视窗建立视窗
+create view "year_average_rating" as
+SELECT year, round(avg("average_rating"),2) as "average_rating"
+from "books_ratings"
+group by year
+
+-- 创建临时视窗
+create temporary view name as
+
+-- common table expression
+with "year_rating" as (
+  select "title", "year", avg(rating) as "average_rating"
+  from books
+  join ratings on "ratings"."book_id" = books.id
+  group by "book_id"
+)
+  
+ select "year", round(avg("average_rating"),2) as "average_rating"
+ from "year_rating"
+ group by "year"
+
+-- 可以使用视窗来对数据进行分组
+
+create view "no_riders" as
+select id, origin, destination, 'anonymous' as "riders" -- 创建新的一列"riders"，用"anonymous"填充
+from rides
+-- 在其它数据库管理系统中，可以设置表格和视窗的访问权限。但是sqlite不可以。
+
+-- 通过视窗功能来进行软删除
+alter table collections
+add column "deleted" integer DEFAULT 0 -- 添加一列来判断数据是否被删除
+
+create view "current_collections" as
+select "id", "title", "accession_number", "acquired"
+from collections
+WHERE deleted = 0
